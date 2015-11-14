a321f4ae3d2f3c12e8556b12e53ba2e5e47ac259
Dominik Schön <ds@grafiniert.de>
2015-11-14 14:04:18 +0100

    Basic setup of reminder mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,5 +5,12 @@ class UserMailer < ApplicationMailer
       @url  = 'http://example.com/login'
       mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+  
+  def reminder_email(user, event_occurrence)
+      @user = user
+      @event = event_occurrence
+      @url  = 'http://example.com/login'
+      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
     
 end
