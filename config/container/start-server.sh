0c5e2fe1f5521c31383cf19a16609e0d122e95d3
Dominik Schön <dominik.schoen@wiwi.uni-regensburg.de>
2015-12-14 09:09:18 +0100

    Asset precompile

--- config/container/start-server.sh
+++ config/container/start-server.sh
@@ -2,5 +2,6 @@
 cd /rails
 source /etc/profile.d/rvm.sh
 bin/rake db:migrate RAILS_ENV=production
+bin/rake assets:precompile RAILS_ENV=production
 bundle exec unicorn -E production -D -p 8080
 nginx
